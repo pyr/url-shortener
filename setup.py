d038828cc33a4f206b9515278844fa3802195c02
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-06-03 10:47:56 +0200

    finalize pypi config

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-`from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as reqs:
     install_requires = [line for line in reqs.read().split('\n') if (
         line and not line.startswith('--'))
     ]
 
-print(find_packages())
+print("found install_requires: " + str(install_requires))
+
 setup(name='url_shortener',
-      version='0.5.4',
+      version='0.5.6',
       description='Simple URL Shortener',
       url='https://github.com/pyr/url-shortener',
       author='Pierre-Yves Ritschard',
       author_email='pyr@spootnik.org',
-      license='Private',
+      license='MIT',
       packages=find_packages(),
       include_package_data=True,
       install_requires=install_requires,
